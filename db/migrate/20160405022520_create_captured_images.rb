dcfa70bb48b5c840449760235f607b21ea89867b
HIRO <takatahirofumi@HIRO-MacBook-Air.local>
2018-04-03 09:38:34 +0900

    create_table
    likeテーブル, tagテーブル prototype_tagテーブル commentテーブル の作成

--- db/migrate/20160405022520_create_captured_images.rb
+++ db/migrate/20160405022520_create_captured_images.rb
@@ -3,7 +3,7 @@ class CreateCapturedImages < ActiveRecord::Migration
     create_table :captured_images do |t|
       t.string :content
       t.integer :status
-      t.references :prototype, index: true, foreign_key: true
+      t.references :prototype, null: false, foreign_key: true
     end
   end
 end
