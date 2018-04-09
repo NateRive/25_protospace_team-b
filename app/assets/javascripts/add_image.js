8eb46d186a812a404b1cae43a97cb3dce57611d0
NateRive <naterive1125@gmail.com>
2018-04-09 10:42:50 +0900

    Finished javascript

--- app/assets/javascripts/add_image.js
+++ app/assets/javascripts/add_image.js
@@ -1,25 +1,49 @@
 $(function(){
-
-  $('content').on('change', 'input[type="file"]', function(e) {
+  $('form').on('change', 'input[type="file"]', function(e) {
     var file = e.target.files[0],
-        reader = new FileReader(),
-        $preview = $(".preview");
-        t = this;
-
+    reader = new FileReader();
+    selector = $(this).parent();
+    selectorImg = $(selector).find("img");
+    if (selectorImg) {
+      $(selectorImg).remove();
+    }
+    $preview = $(selector);
+    t = this;
 
     if(file.type.indexOf("image") < 0){
       return false;
     }
 
+    function includeMain() {
+      var className = selector.attr("class");
+      if (className.match("main")) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
     reader.onload = (function(file) {
       return function(e) {
-        $preview.empty();
-        $preview.append($('<img>').attr({
-                  src: e.target.result,
-                  width: "150px",
-                   class: "preview",
-                  title: file.name
-              }));
+        // $preview.empty();
+
+        if (includeMain()) {
+          $preview.append($('<img>').attr({
+                    src: e.target.result,
+                    width: "690px",
+                    height: "500px",
+                     class: "preview",
+                    title: file.name
+                }));
+        } else {
+          $preview.append($('<img>').attr({
+                    src: e.target.result,
+                    width: "660px",
+                    height: "200px",
+                     class: "preview",
+                    title: file.name
+                }));
+        }
       };
     })(file);
 
