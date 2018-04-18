369d1bf85f85d354f2c88720176528cbd098e878
okada <kirinnremonn1122@gmail.com>
2018-04-18 09:41:37 +0900

    ローカルでは storage :file に

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-    case Rails.env
-    when 'development'
-        config.fog_directory  = '25-apple-pie'
-        config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/25-apple-pie'
-    when 'production'
-        config.fog_directory  = '25-apple-pie'
-        config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/25-apple-pie'
-    end
+  config.fog_directory  = '25-apple-pie'
+  config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/25-apple-pie'
 end
