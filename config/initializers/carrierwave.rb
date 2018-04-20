9c2caa495d0fb70ba9f89ff5e326e9d75a3aa2d9
okada <kirinnremonn1122@gmail.com>
2018-04-20 09:25:02 +0900

    Carrier wave configのコメント解除

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,14 @@
-# CarrierWave.configure do |config|
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-#     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-#     region: 'ap-northeast-1'
-#   }
-#
-#   config.fog_directory  = '25-apple-pie'
-#   config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/25-apple-pie'
-# end
+CarrierWave.configure do |config|
+  case Rails.env
+  when 'production'
+   config.fog_credentials = {
+     provider: 'AWS',
+     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+     region: 'ap-northeast-1'
+   }
+
+   config.fog_directory  = '25-apple-pie'
+   config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/25-apple-pie'
+  end
+end
